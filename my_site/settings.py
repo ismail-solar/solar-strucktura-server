55febc1d8695d37877dccc122b3494d0c9de8cfb
zeshanzee22 <zeshan.z33.z33@gmail.com>
2026-07-15 22:58:14 +0500

    add cors

--- my_site/settings.py
+++ my_site/settings.py
@@ -21,7 +21,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "solar-server-821845790992.us-central1.run.app",
+    "solarstruktura.com",
+    "www.solarstruktura.com",
+]
 
 
 CORS_ALLOW_HEADERS = [
@@ -183,4 +187,14 @@ STORAGES = {
     },
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://solarstruktura.com",
+    "https://www.solarstruktura.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://solarstruktura.com",
+    "https://www.solarstruktura.com",
+]
